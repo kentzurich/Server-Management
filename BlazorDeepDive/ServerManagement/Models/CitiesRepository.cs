133bff81cb80586ca50e7ce9c8962b999e9f4f5f
kentzurich <kentzurichanthony@gmail.com>
2024-06-17 10:54:13 +0800

    Assignment 1: Output city buttons list

---
+++ BlazorDeepDive/ServerManagement/Models/CitiesRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace ServerManagement.Models;
+
+public static class CitiesRepository
+{
+    private static List<string> cities = new List<string>()
+    {
+        "Imus",
+        "Dasmarinas",
+        "Bacoor"
+    };
+
+    public static List<string> GetCities() => cities;
+}
--- BlazorDeepDive/ServerManagement/obj/Debug/net8.0/ServerManagement.AssemblyInfo.cs
+++ BlazorDeepDive/ServerManagement/obj/Debug/net8.0/ServerManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a8ae4c15790d9058b271ade28f8c4d547a8f98e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5b12f8581d25c7549f3d3d45a9dad310d7f8c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
