2ebf42f4f5c9582d75068cfcba642f898962978a
kentzurich <kentzurichanthony@gmail.com>
2024-06-26 21:08:58 +0800

    Highlight current selected City

--- BlazorDeepDive/ServerManagement/obj/Debug/net8.0/ServerManagement.AssemblyInfo.cs
+++ BlazorDeepDive/ServerManagement/obj/Debug/net8.0/ServerManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b21c3c2e69fbfa641644c3e3b1e9a767a7171fa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c98b2743c5cc03ff2b1c62e153f775504d903054")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
